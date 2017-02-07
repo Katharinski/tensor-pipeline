5f64c1e2b9a5b195e498072e3685c0c1b046d426
Katharinski <katharina.glomb@upf.edu>
2017-02-07 14:10:43 +0100

    Update and rename make_tens_kraskov.m to make_tensor.m
    turned this into a function, will work with all three methods of FC computation, which are given as an input variable

---
+++ make_tensor.m
@@ -0,0 +1,27 @@
+% Function to create tensor from multichannel, multisubject data
+
+% inputs: data   - TxNxS matrix (t-time points, N-number of ROIs/channels,
+%                  S-number of subjects)
+%         w      - window size (in frames)
+%         method - 'corr' for correlation coefficient, 'abs' for absolute
+%                  value of correlation coefficient, 'MI' for mutual 
+%                  information according to Kraskov et al., 2004
+%                  further methods can easily be implemented
+
+function tens = make_tensor(data,w,method)
+    [T,N,S] = size(data);
+    W = T-w+1; % number of windows/layers in the tensor
+    tens = zeros(N,N,W,S);
+    for s=1:S
+        switch method
+            case {'corr','abs'}
+                tens(:,:,:,s) = prepdata_fcms(data(:,:,s),w);
+                
+            case 'MI'
+                tens(:,:,:,s) = MI_kraskov(data(:,:,s),w);
+        end
+    end
+    if strcmp(method,'abs')
+        tens = abs(tens);
+    end
+end
